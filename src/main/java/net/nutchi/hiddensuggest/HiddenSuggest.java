63d0811fe8f8c72f5abe4b6ba81b22ef9c98f397
e2318501 <e2318501@outlook.com>
2024-06-21 02:44:25 +0900

    chore: init

---
+++ src/main/java/net/nutchi/hiddensuggest/HiddenSuggest.java
@@ -0,0 +1,24 @@
+package net.nutchi.hiddensuggest;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandSendEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HiddenSuggest extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    @EventHandler
+    public void onPlayerCommandSend(PlayerCommandSendEvent event) {
+        if (!event.getPlayer().hasPermission("hiddensuggest.bypass")) {
+            event.getCommands().clear();
+        }
+    }
+}
